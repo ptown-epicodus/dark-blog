35d83b69110168f4fc0be18d0e351cf886d44b42
Matt Kelley and Patrick McGreevy <patrick@>
2017-03-22 09:34:34 -0700

    Start create post function

--- app/components/add-post.js
+++ app/components/add-post.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
+import moment from 'moment';
+var currentTime = function() {
+  return moment().format('YYYY-MM-DD hh:mm:ss');
+};
 
 export default Ember.Component.extend({
+  actions: {
+    createPost() {
+      var params = {
+        title: this.get('title'),
+        body: this.get('body'),
+        time: currentTime()
+      };
+      this.sendAction('savePost', params);
+    }
+  }
 });
